c70bca4bfbdee9a94826bc7aaa371be175eed921
Brian Lego <legobw@gmail.com>
2020-06-23 12:55:08 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,8 @@
 def find_element_index(array, value_to_find)
   count = 0 
+  found_value = nil 
   while count < array.length do
-    puts array[count]==value_to_find
+    if array[count]==value_to_find
     count += 1 
   end 
   
